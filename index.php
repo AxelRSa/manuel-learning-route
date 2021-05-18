28a9f0941c8c01e7a9ffba056b15e5bbe2f44654
AxelRSa <axelg.rizos@gmail.com>
2021-05-17 19:44:46 -0500

    finishing the fisrt part of the project

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+	<!-- Bootstrap ICONS-->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+	<title>Temario de clases</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<div class="background container-fluid"></div>
+	<div class="container bg-white">
+		<div class="container">
+			<div class="row">
+				<h1 class="text-center">Temario de clases <i class="bi bi-code-square"></i></h1>
+				<hr>
+			</div>
+		</div>
+		<div class="container py-4">
+			<!-- 17/5/21 -->
+			<h4 class="cl-danger">Tarea:</h4>
+			<ul>
+				<li><span class="fw-bold">Terminar: </span><a href="http://www.flexboxdefense.com/"> Flex Defense</a></li>
+				<li><span class="fw-bold">Estudiar: </span>
+					<ul>
+						<li><span class="fw-bold">HTML :</span>
+							<ul>
+								<li>Etiqueta form</li>
+								<li>Etiqueta table</li>
+								<li>Etiqueta video</li>
+								<li>Etiqueta audio</li>
+								<li>Etiqeuta div</li>
+							</ul>
+						</li>
+						<li><span class="fw-bold">Rutas relativas</span></li>
+					</ul>
+				</li>
+			</ul>
+			<h4 class="cl-danger">Pendientes:</h4>
+			<ul>
+				<li>Cómo instalar preprocesadores</li>
+			</ul>
+			<!-- 17/5/21 -->
+		</div>
+
+
+
+	</div>
+
+
+	<!-- Option 1: Bootstrap Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
